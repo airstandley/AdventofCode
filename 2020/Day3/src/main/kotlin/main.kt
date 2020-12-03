621b51595f95b2f5e7210c20a1963e9a563cb033
Andrew Standley <andrew.standley@plangrid.com>
2020-12-02 21:27:17 -0800

    Day 3. Part 1

---
+++ 2020/Day3/src/main/kotlin/main.kt
@@ -0,0 +1,45 @@
+import java.io.File
+
+fun getInput(): List<String> {
+    return File("Input").readLines()
+}
+
+val TREE = '#'
+val GROUND = '.'
+
+class Slope(val grid: List<String>, start: Pair<Int, Int>) {
+    var x = start.first
+    var y = start.second
+
+    fun position(): Char {
+        return this.grid[this.y][this.x]
+    }
+
+    fun atBottom(): Boolean {
+        return this.y == (this.grid.size - 1)
+    }
+
+    fun moveDownSlope(angle: Pair<Int, Int>): Char {
+        val (x, y) = angle
+        this.y += y
+        this.x += x
+        if (this.x >= this.grid[this.y].length) {
+            this.x = this.x - this.grid[this.y].length
+        }
+        return this.position()
+    }
+}
+
+fun countTreesInPath(slope: Slope, angle: Pair<Int, Int>): Int {
+    var count = 0
+    while (!slope.atBottom()) {
+        if (slope.moveDownSlope(angle) == TREE) {
+            count ++
+        }
+    }
+    return count
+}
+
+fun main(args: Array<String>) {
+    println("First Solution: ${countTreesInPath(Slope(getInput(), Pair(0,0)), Pair(3,1))}")
+}
