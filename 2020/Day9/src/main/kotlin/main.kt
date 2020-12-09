b170c4a7aed4a3669ee17009eda6006823d69058
Andrew Standley <andrew.standley@plangrid.com>
2020-12-08 22:11:43 -0800

    Day 9. Part 2

--- 2020/Day9/src/main/kotlin/main.kt
+++ 2020/Day9/src/main/kotlin/main.kt
@@ -1,4 +1,3 @@
-import sun.font.TrueTypeFont
 import java.io.File
 
 fun getInput(): List<String> {
@@ -11,7 +10,7 @@ fun convertInput(input: List<String>): List<Long> {
     return out
 }
 
-fun bruteForce(input: List<Long>, length: Int): Long {
+fun bruteForceInvalid(input: List<Long>, length: Int): Long {
     for(test in length until input.size) {
         var match = false
         for (first in (test - length) until input.size) {
@@ -32,6 +31,40 @@ fun bruteForce(input: List<Long>, length: Int): Long {
     throw Exception("Invalid Code: All numbers in the sequence were valid")
 }
 
+fun bruteForceWeakness(input: List<Long>, key: Long): Long {
+    // Find a continuous sequence that add to key
+    var first: Int = 0
+    var second: Int = 1
+    var sequence: MutableList<Long> = mutableListOf()
+    sequence.add(input[first])
+    sequence.add(input[second])
+    while (first < (input.size - 2) && second < (input.size - 1)) {
+        val sum = sequence.reduce { acc, it -> acc + it }
+        when {
+            sum == key -> {
+                // We've found the sequence
+                sequence.sort()
+                return sequence[0] + sequence.last()
+            }
+            sum > key -> {
+                // Move the window floor
+                first ++
+                sequence = sequence.drop(1) as MutableList<Long>
+            }
+            else -> {
+                // Move the window ceiling
+                second ++
+                sequence.add(input[second])
+            }
+        }
+    }
+    throw Exception("Invalid Code: Unable to find sequence.")
+}
+
 fun main(args: Array<String>) {
-    println("First Solution: ${bruteForce(convertInput(getInput()), 25)}")
+    val input = convertInput(getInput())
+    val key = bruteForceInvalid(input, 25)
+    println("First Solution: $key")
+    val weakness = bruteForceWeakness(input, key)
+    println("Second Solution $weakness")
 }
